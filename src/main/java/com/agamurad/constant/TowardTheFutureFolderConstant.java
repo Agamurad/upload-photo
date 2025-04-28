b1e7023110b8139d73284bd7387df89e7ebdc043
Aghamurad Najafov <agamuradn@gmail.com>
2025-04-28 15:50:42 +0400

    first commit

--- src/main/java/com/agamurad/constant/TowardTheFutureFolderConstant.java
+++ src/main/java/com/agamurad/constant/TowardTheFutureFolderConstant.java
@@ -2,5 +2,6 @@ package com.agamurad.constant;
 
 public class TowardTheFutureFolderConstant {
 
-	 public static final String TOWARD_THE_FUTURE_FOLDER_UPLOAD = "D:\\Spring Boot Projects\\upload-photo\\src\\main\\resources\\uploads";
+	 //public static final String TOWARD_THE_FUTURE_FOLDER_UPLOAD = "D:\\Spring Boot Projects\\upload-photo\\src\\main\\resources\\uploads";
+	public static final String TOWARD_THE_FUTURE_FOLDER_UPLOAD = System.getProperty("user.dir") + "/uploads";
 }
